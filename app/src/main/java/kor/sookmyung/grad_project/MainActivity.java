645e57fa6e2fc62e3766e1e3a3a83835b2ba1709
soo <tnalsdh21@gmail.com>
2022-02-15 13:48:51 +0900

    파이어베이스 호스팅까지

--- app/src/main/java/kor/sookmyung/grad_project/MainActivity.java
+++ app/src/main/java/kor/sookmyung/grad_project/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StorageReference mStorageRef;
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
+
+
+
 }
